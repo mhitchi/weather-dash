2d004987d6df88c4a1eee52530e91e4f47b635ca
mhitchi <mayhitchings@gmail.com>
2020-01-13 18:25:46 -0500

    separated more functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,27 +5,33 @@ $(document).ready(function(){
     var city = $('#city').val();
     var apikey = 'a01adedec966523f1f3f3cd3a1d8d9b3';
 
+    var today = moment().format('MMM Do, YYYY');
+    var currentCity = $('<h1>');
+    var currentDate = $('<h2>');
+    var currentIcon = $('<img>');
+    var currentIconURL = '';
+    var currentTemp = $('<p>');
+    var currentHumidity = $('<p>');
+    var currentWind = $('<p>');
     var currentLat;
     var currentLon;
     var currentTimeCode;
 
     // if there's a city typed in
     if( city !== "" ){
-      // get data from API
+      getData(city);
+    }
+    // get data from API
+    function getData(city){
       $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/weather',
-        //url: 'https://api.openweathermap.org/data/2.5/weather?'+ city + apikey + '&units=imperial',
         dataType: 'json',
         type: 'GET',
         data: {
           q: city,
           appid: apikey,
           units: 'imperial'
-        },
-        // success: function(response) {
-        //   console.log(response.data);
-        // }
-
+        }
       }).then(function(response) {
         console.log(response);
         // for(var i=0; i<response.data.length; i++) {
@@ -50,61 +56,47 @@ $(document).ready(function(){
 
         getUVI();
       });
-
-      function createEl(response){
-        // show current city
-        // show current date
-        // show current icon, temp, humidity, wind speed
-        var currentCity = $('<h1>');
-        currentCity.text(city);
-
-        var currentDate = $('<h2>');
-        var today = moment().format('MMM Do, YYYY');
-        currentDate.text(today);
-
-        var currentIcon = $('<img>');
-        var currentIconURL = "http://openweathermap.org/img/w/" + (response.weather[0].icon) + ".png";
-        currentIcon.attr('src', currentIconURL);
-
-        var currentTemp = $('<p>');
-        currentTemp.text((response.main.temp) + ' degrees Farenheit');
-
-        var currentHumidity = $('<p>');
-        currentHumidity.text((response.main.humidity) + '%');
-
-        var currentWind = $('<p>');
-        currentWind.text(response.wind.speed);
-
-        updateDOM(currentCity, currentDate, currentIcon, currentIconURL, currentTemp, currentHumidity, currentWind);
-      }
-
-      //append elements to DOM
-      function updateDOM(currentCity, currentDate, currentIcon, currentIconURL, currentTemp, currentHumidity, currentWind){
-        $('#current').append(currentCity);
-        $('#current').append(currentDate);
-        $('#current').append(currentIcon);
-        $('#current').append(currentTemp);
-        $('#current').append(currentHumidity);
-        $('#current').append(currentWind);
-      }
-
-      // get UV index
-      function getUVI() {
-        $.ajax({
-          url: 'https://api.openweathermap.org/data/2.5/uvi',
-          dataType: 'json',
-          type: 'GET',
-          data: {
-            appid: apikey,
-            lat: currentLat,
-            lon: currentLon
-          }
-        }).then(function(response) {
-          var currentUV = $('<p>');
-          currentUV.text(response.value);
-          $('#current').append(currentTemp);
-        })
-      }
     }
-  })
-})
+
+    function createEl(response){
+      // show current city, date, icon, temp, humidity, wind speed
+      currentCity.text(city);
+      currentDate.text(today);
+      currentIconURL = "http://openweathermap.org/img/w/" + (response.weather[0].icon) + ".png";
+      currentIcon.attr('src', currentIconURL);
+      currentTemp.text((response.main.temp) + ' degrees Farenheit');
+      currentHumidity.text((response.main.humidity) + '%');
+      currentWind.text(response.wind.speed);
+
+      updateDOM(currentCity, currentDate, currentIcon, currentIconURL, currentTemp, currentHumidity, currentWind);
+    }
+
+    //append elements to DOM
+    function updateDOM(currentCity, currentDate, currentIcon, currentIconURL, currentTemp, currentHumidity, currentWind){
+      $('#current').append(currentCity);
+      $('#current').append(currentDate);
+      $('#current').append(currentIcon);
+      $('#current').append(currentTemp);
+      $('#current').append(currentHumidity);
+      $('#current').append(currentWind);
+    }
+
+    // get UV index
+    function getUVI() {
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/uvi',
+        dataType: 'json',
+        type: 'GET',
+        data: {
+          appid: apikey,
+          lat: currentLat,
+          lon: currentLon
+        }
+      }).then(function(response) {
+        var currentUV = $('<p>');
+        currentUV.text(response.value);
+        $('#current').append(currentTemp);
+      })
+    }
+  })//on click
+})//doc get ready
