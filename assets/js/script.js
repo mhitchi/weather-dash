d9db8f4f99de09dfed43b8b24c3c2667cf2dc3de
mhitchi <mayhitchings@gmail.com>
2020-01-14 09:44:10 -0500

    troubleshooting sidebar city button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,19 +12,20 @@ $(document).ready(function(){
   var currentLat;
   var currentLon;
   var currentTimeCode;
+
+  var apikey = 'a01adedec966523f1f3f3cd3a1d8d9b3';
+
   // on click, get city name
   $('#getWeather').on('click', function(e) {
     e.preventDefault();
     var city = $('#city').val();
-    var apikey = 'a01adedec966523f1f3f3cd3a1d8d9b3';
-
-
-
+  
     // if there's a city typed in
-    if( city !== "" ){
+    // if( city !== "" ){
       getData(city);
       addCity(city);
-    }
+    // }
+  })//on click
     // get data from API
     function getData(city){
       $.ajax({
@@ -49,9 +50,11 @@ $(document).ready(function(){
         currentLat = response.coord.lat;
         currentLon = response.coord.lon;
         currentTimeCode = response.dt;
+        var currentUV = $('<p>');
+        currentUV.text("");
 
         createEl(response)
-        getUVI(currentLat, currentLon);
+        getUVI(currentLat, currentLon, currentUV);
 
         //TODO NOT WORKING
         //get description, get background image respectively
@@ -63,7 +66,7 @@ $(document).ready(function(){
 
     function createEl(response){
       // show current city, date, icon, temp, humidity, wind speed
-      currentCity.text(city);
+      currentCity.text($('#city').val());
       currentDate.text(today);
       currentIconURL = "http://openweathermap.org/img/w/" + (response.weather[0].icon) + ".png";
       currentIcon.attr('src', currentIconURL);
@@ -85,7 +88,8 @@ $(document).ready(function(){
     }
 
     // TODO get UV index
-    function getUVI(currentLat, currentLon) {
+    function getUVI(currentLat, currentLon, currentUV) {
+      
       $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/uvi',
         dataType: 'json',
@@ -96,32 +100,84 @@ $(document).ready(function(){
           lon: currentLon
         }
       }).then(function(response) {
-        // console.log(response);
-        var currentUV = $('<p>');
+        
         currentUV.text("UV index: " + (response.value));
         $('#current').append(currentUV);
       })
     }
-  })//on click
+
   //add city to array
   function addCity(city) {
     var cityList = $('#city-list');
     cityArr.pop(city);
+    //RESULT no list items showing
     // $.each(cityArr, function(index, value) {
-        // var listItem = $('<li>');
-        // listItem = cityArr[i]
-        // cityList.prepend(listItem);
+    //     var listItem = $('<li>');
+    //     listItem = cityArr[i]
+    //     cityList.prepend(listItem);
     // });
     // for( var i = 0; i < cityArr.length; i++ ) {
     //   var listItem = $('<li>');
     //   listItem.text(cityArr[i]);
     //   cityList.prepend(listItem);
     // }
-    cityList.prepend($('<li>', {
-      text: city,
-      class: 'button',
-      type: 'button'
-    }));
+    // TODO NOT WORKING
+    // RESULT on click not working
+      // cityList.prepend($('<li>', {
+      //   text: city,
+      //   class: 'list-button',
+      //   type: 'button'
+      // })).on('click', function() {
+      //   for( var i = 0; i < cityArr.length; i++ ) {
+      //     var listItem = cityArr[i].text();
+      //     console.log(listItem);
+      //   }
+      // });
 
-  }
+      var listItem = $('<li>');
+      listItem.addClass('list-button');
+      listItem.attr('type', 'button');
+      listItem.text(city);
+      cityList.prepend(listItem);
+
+      listItem.on('click', function() {
+        //$('#current').empty();
+        getData(city);
+      });
+    //}
+  // TRIED putting the whole thing in a for loop
+  // RESULT no list items rendering
+  //   for( var i = 0; i < cityArr.length; i++ ) {
+  //     cityList.prepend($('<li>', {
+  //       text: city,
+  //       class: 'list-button',
+  //       type: 'button'
+  //     })).on('click', function() {
+  //         var listItem = cityArr[i].text();
+  //         console.log(listItem);
+  //       });
+  //   }
+ }
+
+    // function addCity() {
+    //   var cityList = $("#city-list");
+    //   for (var i = 0; i < cityArr.length; i++) {
+    //     //creat list item
+    //     var listItem = $("<li>");
+    //     // add attributes and event listener
+    //     listItem.text(cityArr[i]);
+    //     listItem.attr("value", cityArr[i]);
+    //     listItem.attr("class", "list-group-item");
+    //     listItem.attr("type", "button");
+    //     listItem.removeClass("active");
+    //     // pass user click to query to re-render card states
+    //     listItem.on("click", function () {
+    //         $(this).attr("class", "list-group-item active");
+    //         addCity();
+    //         city = $(this).attr("value");
+    //         getData();
+    //     });
+    //     cityList.prepend(listItem);
+    //   }
+    // }
 })//doc get ready
